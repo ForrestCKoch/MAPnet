997164e617d0dc3cdbfceec8cfd15ac0124e4d9a
Forrest Koch <forrest.koch@unsw.edu.au>
2019-07-07 22:41:18 +1000

    saving work

--- map/train.py
+++ map/train.py
@@ -2,13 +2,18 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader
 
-def train(dataset, model, epochs=10, update_freq=1, batch_size=8, num_workers=1, 
-            cuda=False, loss_func=None, optimizer=None, scheduler=None):
+def train(train, test, model, epochs=10, update_freq=5, batch_size=8, 
+    num_workers=1, cuda=False, loss_func=None, optimizer=None, scheduler=None,):
 
-    data_loader = DataLoader(dataset, num_workers=num_workers,
+    train_data_loader = DataLoader(train, num_workers=num_workers,
                     pin_memory=cuda, batch_size=batch_size,
                     shuffle=True)
-    desc_genr = lambda x,y: 'Epoch: {} Loss: {:.2f}'.format(x,y)
+
+    test_data_loader = DataLoader(test, num_workers=num_workers,
+                    pin_memory=cuda, batch_size=batch_size,
+                    shuffle=True)
+    desc_genr = lambda x,y,z: 'Epoch: {} Test Loss: {:.2f} Train Loss: {:.2f}'.format(x,y,z)
+    test_loss = 0.0
 
     if cuda:
         model = model.cuda()
@@ -22,11 +27,14 @@ def train(dataset, model, epochs=10, update_freq=1, batch_size=8, num_workers=1,
 
     model_optimizer = optimizer(model.parameters())
     model_scheduler = scheduler(model) if scheduler is not None else scheduler
+    
 
     for i in range(0, epochs):
+
         if model_scheduler is not None:
             model_scheduler.step()
-        data_iterator = tqdm(data_loader,desc=desc_genr(0,0),unit='batch')
+        data_iterator = tqdm(train_data_loader,desc=desc_genr(i,test_loss,0))
+
         for index, batch in enumerate(data_iterator):
             x,label = batch
             if cuda:
@@ -37,10 +45,25 @@ def train(dataset, model, epochs=10, update_freq=1, batch_size=8, num_workers=1,
                 label = label.float()
             y = model(x)
             loss = loss_func(y,label.view(-1,1))
-            loss_value = float(loss.item())
+            loss_value = float(loss.item())/batch_size
             model_optimizer.zero_grad()
             loss.backward()
             model_optimizer.step()  
-            data_iterator.set_description(desc_genr(i,loss_value))
+            data_iterator.set_description(desc_genr(i,test_loss,loss_value))
     
+        if (i+1)%update_freq==0:
+            total_loss = 0.0
+            for index, batch in enumerate(test_data_loader):
+                x,label = batch
+                if cuda:
+                    x = x.cuda().float()
+                    label = label.cuda().float()
+                else:
+                    x = x.float()
+                    label = label.float()
+                y = model(x)
+                loss = loss_func(y,label.view(-1,1))
+                total_loss += float(loss.item())/batch_size
+            test_loss = total_loss/(index+1)
+            
     
--- tests/test_train.py
+++ tests/test_train.py
@@ -3,11 +3,14 @@ from map.model import *
 from map.train import *
 from torch.utils.data import DataLoader
 
-sd = get_sample_dict('data',dataset='train')
-ages = get_sample_ages(sd.keys(),'data/subject_info.csv')
+train_dict = get_sample_dict('data',dataset='train')
+train_ages = get_sample_ages(train_dict.keys(),'data/subject_info.csv')
+train_ds = NiftiDataset(train_dict,train_ages)
 
-ds = NiftiDataset(sd,ages)
+test_dict = get_sample_dict('data',dataset='train')
+test_ages = get_sample_ages(test_dict.keys(),'data/subject_info.csv')
+test_ds = NiftiDataset(test_dict,test_ages)
 
-model = MAPnet(ds.image_shape)
+model = MAPnet(train_ds.image_shape)
 
-train(ds,model,cuda=True,batch_size=8,num_workers=8,epochs=50)
+train(train_ds,test_ds,model,cuda=True,batch_size=8,num_workers=8,epochs=50,update_freq=10)
