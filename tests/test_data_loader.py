2bbc18c92b79983bf7010993c4bd4a6e85627d74
Forrest Koch <forrest.koch@unsw.edu.au>
2019-07-07 15:29:28 +1000

    add cuda support to test

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -6,7 +6,7 @@ sd = get_sample_dict('data',dataset='train')
 ages = get_sample_ages(sd.keys(),'data/subject_info.csv')
 
 ds = NiftiDataset(sd,ages)
-dl = DataLoader(ds,num_workers=8)
+dl = DataLoader(ds,num_workers=8,pin_memory=True,batch_size=32)
 
 for i in range(0,10):
     for batch,sample in tqdm(enumerate(dl)):
