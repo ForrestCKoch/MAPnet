840c16a465e21e5701b12ef8784eb1163e5620bd
Forrest Koch <forrest.koch@unsw.edu.au>
2019-07-07 21:15:06 +1000

    add module tests

---
+++ tests/test_model.py
@@ -0,0 +1,18 @@
+from map.data import *
+from map.model import *
+from torch.utils.data import DataLoader
+
+sd = get_sample_dict('data',dataset='train')
+ages = get_sample_ages(sd.keys(),'data/subject_info.csv')
+
+ds = NiftiDataset(sd,ages)
+dl = DataLoader(ds,num_workers=8,pin_memory=False,batch_size=1)
+
+x,l = ds[0]
+
+model = MAPnet(ds.image_shape)
+model = model.float()
+
+for batch,labels in dl:
+    y = model.forward(batch.float())
+    print(y)
---
+++ tests/test_train.py
@@ -0,0 +1,13 @@
+from map.data import *
+from map.model import *
+from map.train import *
+from torch.utils.data import DataLoader
+
+sd = get_sample_dict('data',dataset='train')
+ages = get_sample_ages(sd.keys(),'data/subject_info.csv')
+
+ds = NiftiDataset(sd,ages)
+
+model = MAPnet(ds.image_shape)
+
+train(ds,model,cuda=True)
