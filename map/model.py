abb84b463a9096aa84ff56b561ce5e468095e361
Forrest <forrest.c.koch@gmail.com>
2019-07-12 08:42:48 +1000

    documentation

--- map/model.py
+++ map/model.py
@@ -1,14 +1,16 @@
-from typing import Any, Optional, Callable
+from typing import Any, Optional, Callable, Tuple
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
 
-def get_out_dims(inputs: np.ndarray,
-                 padding: np.ndarray,
-                 dilation: np.ndarray,
-                 kernel: np.ndarray,
-                 stride: np.ndarray)->np.ndarray:
+def get_out_dims(
+        inputs: np.ndarray,
+        padding: np.ndarray,
+        dilation: np.ndarray,
+        kernel: np.ndarray,
+        stride: np.ndarray
+    )->np.ndarray:
     """
     calculate the output dimensions of a Conv3d layer
     :param inputs: 3 element vector for shape of input
@@ -17,43 +19,58 @@ def get_out_dims(inputs: np.ndarray,
     :param kernel: 3 element vector for kernel parameters
     :param stride: 3 element vector for stride parameters 
     """
-    
     return np.floor(1+(inputs-1+(2*padding)-(dilation*(kernel-1)))/stride)
 
 class MAPnet(nn.Module):
 
-    def __init__(self,input_shape: tuple):
+    def __init__(
+            self, 
+            input_shape: Tuple[int,int,int],
+            n_conv_layers: int = 3,
+            padding: int = 0,
+            dilation: int = 3,
+            kernel: int = 3,
+            stride: int = 3,
+            filters: List[int] = [4,4,4],
+            input_channels: int = 1,
+        ):
         """
-        Initialize an instance of MAPnet
-        :param input_shape: shape of input image
+        Initialize an instance of MAPnet.
+        :param input_shape: shape of input image.
+        :param n_conv_layers: number of `nn.Conv3d` layers to use.
+        :param padding: `padding` parameter for `nn.Conv3d`.
+        :param dilation: `dilation` parameter for `nn.Conv3d`.
+        :param kernel: `kernel_size` parameter for `nn.Conv3d`.
+        :param stride: `stride` parameter for `nn.Conv3d`.
+        :param filters: List of filters per layer.
+        :param input_channels: Number of input channels to the model. 
         """
+        
         super(MAPnet,self).__init__()
         self.conv_layer_sizes = list([np.array(input_shape)])
-        for i in range(0,3):
+        for i in range(0,n_conv_layers):
             self.conv_layer_sizes.append(
                 get_out_dims(
                     self.conv_layer_sizes[-1], # input dimensions    
-                    np.repeat(0,3), # padding
-                    np.repeat(1,3), # dilation
-                    np.repeat(3,3), # kernel
-                    np.repeat(3,3), # stride
+                    np.repeat(padding,3), # padding
+                    np.repeat(dilation,3), # dilation
+                    np.repeat(kernel,3), # kernel
+                    np.repeat(stride,3), # stride
                 )
             )
-        print(self.conv_layer_sizes)
 
-        n_filters = [4,4,4]
         conv_layers = list()
-        self.n_channels=list([4])
-        for i in range(0,3):
-            self.n_channels.append(self.n_channels[-1] * n_filters[i])
+        self.n_channels=list([input_channels])
+        for i in range(0,n_conv_layers):
+            self.n_channels.append(self.n_channels[-1] * filters[i])
             conv_layers.append(
                 nn.Conv3d(
                     in_channels=self.n_channels[-2], 
                     out_channels=self.n_channels[-1], 
-                    kernel_size=3, 
-                    stride=3, 
-                    padding=0, 
-                    dilation=1, 
+                    kernel_size=kernel, 
+                    stride=stride, 
+                    padding=padding, 
+                    dilation=dilation, 
                     groups=1, 
                     bias=True, 
                     padding_mode='zeros'
