f150e91b8fca532add04df903f76df31620239c1
Forrest Koch <forrest.koch@unsw.edu.au>
2019-07-12 07:34:25 +1000

    modify test script to use dwi dataset

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,14 +3,18 @@ from map.model import *
 from map.train import *
 from torch.utils.data import DataLoader
 
-train_dict = get_sample_dict('data',dataset='train')
-train_ages = get_sample_ages(train_dict.keys(),'data/subject_info.csv')
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+train_dict = get_sample_dict('dwi_data',dataset='train')
+train_ages = get_sample_ages(train_dict.keys(),'dwi_data/subject_info.csv')
 train_ds = NiftiDataset(train_dict,train_ages)
 
-test_dict = get_sample_dict('data',dataset='train')
-test_ages = get_sample_ages(test_dict.keys(),'data/subject_info.csv')
+test_dict = get_sample_dict('dwi_data',dataset='train')
+test_ages = get_sample_ages(test_dict.keys(),'dwi_data/subject_info.csv')
 test_ds = NiftiDataset(test_dict,test_ages)
 
 model = MAPnet(train_ds.image_shape)
+#print(count_parameters(model))
 
-train(train_ds,test_ds,model,cuda=True,batch_size=8,num_workers=8,epochs=50,update_freq=10)
+train(train_ds,test_ds,model,cuda=True,batch_size=32,num_workers=8,epochs=100,update_freq=1)
