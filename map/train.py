1171c7131a53c08bfa25b4569bb59688efdacca2
Forrest Koch <forrest.koch@unsw.edu.au>
2019-07-21 10:53:00 +1000

    change tqdm formatting

--- map/train.py
+++ map/train.py
@@ -62,7 +62,7 @@ def train_model(
             pin_memory=cuda, batch_size=batch_size,
             shuffle=True)
 
-    desc_genr = lambda x,y,z,lr: 'Epoch: {} Test Loss: {} Train Loss: {} LR: {}'.format(
+    desc_genr = lambda x,y,z,lr: 'Epoch: {0:3} Test Loss: {1:9} Train Loss: {2:9} LR: {3:9}'.format(
         x,
         np.format_float_scientific(y, precision=3),
         np.format_float_scientific(z, precision=3),
